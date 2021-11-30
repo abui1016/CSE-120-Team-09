bd4a69512459dcc35a16208ef4076e833a32ae26
Brodey-Nance <brodeynance@gmail.com>
2021-11-30 07:01:16 -0800

    Added libraries and updated with Andys file changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,11 @@
-// gets time for the timestamps.
-function getTime() {
-  const date = new Date();
-  const myDate = new Date();
-  // get hour value.
-  let hours = myDate.getHours();
-  const ampm = hours >= 12 ? "Pm" : "Am";
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  let minutes = myDate.getMinutes();
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  const myTime = hours + " " + ampm + " : " + minutes;
-  return myTime;
-}
-
 // Import
 const mysql = require("mysql");
 // const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const url = require("url");
-
+const alert = require("alert");
+const schedule = require("node-schedule");
 
 const saltRounds = 10;
 
@@ -51,59 +37,21 @@ transporter.verify(function (error, success) {
   }
 });
 
-// Email Templates
-const optionsRegister = {
-  from: process.env.SES_FROM,
-  to: process.env.SES_TO,
-  subject: "EFM : Account Registered",
-  text:
-    "Hello , " +
-    " User \n You are now registered user for Early Family Math if you wish to get started please follow the link to setup content delivery. We have also attached a form to this email that is a quick start guide to walk you through setup. ",
-  attachments: [
-    {
-      path: "test.txt",
-    },
-  ],
-};
-
-const optionsLogin = {
-  from: process.env.SES_FROM,
-  to: process.env.SES_TO,
-  subject: "Early Family Math User Login",
-  text: "User has successfully logged in at " + ".",
-};
-
-const optionsSettingChange = {
-  from: process.env.SES_FROM,
-  to: process.env.SES_TO,
-  subject: "Nodemailer Login",
-  text: "A user has successfully logged in at " + getTime(),
-  // attachments: [
-  //     {
-  //       path: 'directory/filename'
-  //     },
-  //   ],
-};
-
-const optionsActivities_3 = {
-  from: process.env.SES_FROM,
-  to: process.env.SES_TO,
-  subject: "EFM Chapter 3 Activities",
-  text: "2 Daily activities from chapter 3",
-  attachments: [{ path: "Activities/3-1.pdf" }],
-};
+// Send out daily activities
+// Sends out activity at every 45th minute of each hour when running
+const job = schedule.scheduleJob("45 * * * *", sendActivities);
 
 // Email functions
 function sendMailRegister(user) {
-  console.log(user.emailAddress);
+  // console.log(user.emailAddress);
   optionsRegister = {
     from: process.env.SES_FROM,
     to: user.emailAddress,
     subject: "EFM : Account Registered",
     text:
-      "Hello , " +
+      "Hello " +
       user.firstName +
-      '"n You are now a registered user with Early Family Math. If you wish to get started please login to the Early Fmaily Math portal. Once logged in you may begin setup for conetnt delivery. ',
+      ",\n You are now a registered user with Early Family Math. You will automatically receive daily emails with 2 activities for your child. If you feel that the pace is too slow or fast, you may log in to your account to adjust your child's skill level.",
   };
 
   transporter.sendMail(optionsRegister, function (err, info) {
@@ -115,86 +63,96 @@ function sendMailRegister(user) {
   });
 }
 
-function sendMailLogin() {
-  transporter.sendMail(optionsLogin, function (err, info) {
-    if (err) {
-      console.log(err);
-      return;
+// Function to send out activities
+function sendActivities() {
+  db.query(
+    "SELECT * FROM users WHERE subscribed = 'TRUE'",
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      }
+      for (let i = 0; i < results.length; i++) {
+        const optionsActivities = {
+          from: process.env.SES_FROM,
+          to: results[i].emailAddress,
+          subject: "EFM Daily Activity",
+          text: `Hello ${results[i].firstName} \nHere are two daily activities based on your child's skill level (${results[i].skillLevel})!`,
+          attachments: [
+            {
+              path: `./Activities/${results[i].skillLevel}-${results[i].activityLevel}.pdf`,
+            },
+            {
+              path: `./Activities/${results[i].skillLevel}-${results[i]
+                .activityLevel++}.pdf`,
+            },
+          ],
+        };
+        transporter.sendMail(optionsActivities, function (err, info) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("Sent Activities!");
+        });
+      }
     }
-    console.log("Sent: " + info.response);
-  });
+  );
+  updateSkillLevel();
 }
 
-function sendMailSettingChange() {
-  transporter.sendMail(optionsSettingChange, function (err, info) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("Sent: " + info.response);
-  });
-}
-
-// Sample function to send out weekly emails
-// Get emails
-// results is an array, i.e. results[0] is first email
-const oneDay = 86400000;
-const oneWeek = 604800000;
-function sendWeeklyEmail() {
-  db.query("SELECT emailAddress, skillLevel FROM users", (error, results) => {
-    if (error) {
-      console.log(error);
-    }
-    sendActivities(results);
-    // for (let i = 0; i < results.length; i++) {
-    //   // send emails here
-    //   // console.log(results[i]);
-    // }
-  });
-}
-
-function sendActivities(users) {
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].skillLevel === 3) {
-      transporter.sendMail(optionsActivities_3, function (err, info) {
-        if (err) {
-          console.log(err);
+// Function to update skill and activity levels
+function updateSkillLevel() {
+  db.query(
+    "SELECT * FROM users WHERE subscribed = 'TRUE' ",
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      }
+      for (let i = 0; i < results.length; i++) {
+        if (results[i].activityLevel >= 13) {
+          db.query(
+            "UPDATE users SET skillLevel = skillLevel + 1, activityLevel = 1 ",
+            (error, results) => {
+              if (error) {
+                console.log(error);
+              }
+            }
+          );
+        } else {
+          db.query(
+            "UPDATE users SET activityLevel = activityLevel + 2",
+            (error, results) => {
+              if (error) {
+                console.log(error);
+              }
+            }
+          );
         }
-        console.log("Sent: " + info.response);
-      });
+      }
     }
-    // switch (users[i].skillLevel) {
-    //   case 1:
-    //     console.log("Send ch.1 Emails");
-    //     break;
-    //   case 2:
-    //     console.log("Send ch.2 Emails");
-    //     break;
-    //   case 3:
-    //     transporter.sendMail(optionsActivities_3, function (err, info) {
-    //       if (err) {
-    //         console.log(err);
-    //         return;
-    //       }
-    //       console.log("Sent: " + info.response);
-    //     });
-    //     break;
-    //   case 4:
-    //     console.log("Send ch.4 Emails");
-    //     break;
-    //   case 5:
-    //     console.log("Send ch.5 Emails");
-    //     break;
-    // }
-  }
+  );
 }
 
-// Set interval so function is called everytime after time expires
-// setInterval(sendWeeklyEmail, 10000);
+// Get skill level
+function getSkillLevel(id) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "SELECT skillLevel from users WHERE id = ?",
+      [id],
+      (error, results) => {
+        if (error) {
+          return reject(err);
+        }
+        let skill = results[0].skillLevel;
+        resolve(skill);
+      }
+    );
+  });
+}
 
 // Register user to database Pushes the emails and the info to the DB.
 exports.register = (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   //   const name = req.body.name;
   //   const email = req.body.email;
@@ -224,14 +182,12 @@ exports.register = (req, res) => {
       // If it gets a result from the DB that matches at all then it will send out the prompt
 
       if (results.length > 0) {
-        return res.render("register", {
-          message: "That email is already in use",
-        });
+        alert("That email is already in use");
+        return res.redirect("http://localhost:3304/register");
         // Authenticates Passwords
       } else if (password !== passwordConfirm) {
-        return res.render("register", {
-          message: "Passwords do not match",
-        });
+        alert("Passwords did not match");
+        return res.redirect("http://localhost:3304/register");
       }
       // let hashedPassword = await bcrypt.hash(password, 8)
       // console.log(hashedPassword);
@@ -255,11 +211,10 @@ exports.register = (req, res) => {
           if (error) {
             console.log(error);
           } else {
-            console.log(results);
+            // console.log(results);
             sendMailRegister(req.body);
-            return res.render("register", {
-              message: "User registered!",
-            });
+            alert("Sucessfully Registered!");
+            return res.redirect("http://localhost:3304");
           }
         }
       );
@@ -267,7 +222,8 @@ exports.register = (req, res) => {
   );
 };
 
-// Login user  Querey the DB for both email and login.
+// Login user
+// Query the DB for both email and password.
 exports.login = (req, res) => {
   const { emailAddress, password } = req.body;
 
@@ -278,6 +234,10 @@ exports.login = (req, res) => {
     (error, results) => {
       if (error) {
         console.log(error);
+      }
+      if (results.length === 0) {
+        alert("Invalid Login Credentials");
+        return res.redirect("http://localhost:3304/login");
       }
       if (bcrypt.compareSync(password, results[0].password)) {
         // Query DB to match an email and password
@@ -290,8 +250,6 @@ exports.login = (req, res) => {
               console.log(error);
             }
             if (results.length === 1) {
-              sendMailLogin();
-              // sendWeeklyEmail();
               return res.render("editInfo", {
                 firstName: results[0].firstName,
                 lastName: results[0].lastName,
@@ -302,19 +260,21 @@ exports.login = (req, res) => {
                 id: results[0].id,
               });
             } else {
-              return res.render("login", {
-                message: "Invalid login credentials",
-              });
+              alert("Invalid Login Credentials");
+              return res.redirect("http://localhost:3304/login");
             }
           }
         );
+      } else {
+        alert("Invalid Login Credentials");
+        return res.redirect("http://localhost:3304/login");
       }
     }
   );
 };
 
 exports.editInfo = (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const {
     firstName,
     lastName,
@@ -326,45 +286,62 @@ exports.editInfo = (req, res) => {
     id,
   } = req.body;
   // Query into DB and UPDATE
-  if (password === "" && passwordConfirm === "") {
-    db.query(
-      "UPDATE users SET firstName = ?, lastName = ?, emailAddress = ?, phoneNumber = ?, skillLevel = ? WHERE id = ?",
-      [firstName, lastName, emailAddress, phoneNumber, skillLevel, id],
-      (error, results) => {
-        if (error) {
-          console.log(error);
-        }
-        return res.redirect("http://localhost:3304");
+  // Reset activity level if user changes skill level
+  getSkillLevel(id).then((value) => {
+    const prevSkillLevel = value;
+    if (password === "" && passwordConfirm === "") {
+      if (prevSkillLevel !== skillLevel) {
+        db.query(
+          "UPDATE users SET activityLevel = 1 WHERE id = ?",
+          [id],
+          (error, results) => {
+            if (error) {
+              console.log(error);
+            }
+          }
+        );
       }
-    );
-  } else if (
-    password === passwordConfirm &&
-    password !== "" &&
-    passwordConfirm !== ""
-  ) {
-    const hashedPassword = bcrypt.hashSync(password, saltRounds);
-    db.query(
-      "UPDATE users SET firstName = ?, lastName = ?, emailAddress = ?, phoneNumber = ?, password = ?, skillLevel = ? WHERE id = ?",
-      [
-        firstName,
-        lastName,
-        emailAddress,
-        phoneNumber,
-        hashedPassword,
-        skillLevel,
-        id,
-      ],
-      (error, results) => {
-        if (error) {
-          console.log(error);
+      db.query(
+        "UPDATE users SET firstName = ?, lastName = ?, emailAddress = ?, phoneNumber = ?, skillLevel = ? WHERE id = ?",
+        [firstName, lastName, emailAddress, phoneNumber, skillLevel, id],
+        (error, results) => {
+          if (error) {
+            console.log(error);
+          }
+          alert("Successfully updated information");
+          return res.redirect("http://localhost:3304");
         }
-        return res.redirect("http://localhost:3304");
-      }
-    );
-  } else {
-    // Password and password confirm did not match
-    console.log("Passwords did not match");
-    alert("Passwords did not match");
-    return res.redirect("http://localhost:3304");
-  }
+      );
+    } else if (
+      password === passwordConfirm &&
+      password !== "" &&
+      passwordConfirm !== ""
+    ) {
+      const hashedPassword = bcrypt.hashSync(password, saltRounds);
+      db.query(
+        "UPDATE users SET firstName = ?, lastName = ?, emailAddress = ?, phoneNumber = ?, password = ?, skillLevel = ? WHERE id = ?",
+        [
+          firstName,
+          lastName,
+          emailAddress,
+          phoneNumber,
+          hashedPassword,
+          skillLevel,
+          id,
+        ],
+        (error, results) => {
+          if (error) {
+            console.log(error);
+          }
+          alert("Successfully updated information");
+          return res.redirect("http://localhost:3304");
+        }
+      );
+    } else {
+      // Password and password confirm did not match
+      console.log("Passwords did not match");
+      alert("Passwords did not match");
+      return res.redirect("http://localhost:3304");
+    }
+  });
 };
